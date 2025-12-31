ece9d930ab44b8860a4613088c3f3852c6034e1c
Fredy Max <ch4v3zfredy@gmail.com>
2025-12-30 19:20:16 -0600

    Remove error handling logic from GroupPage to streamline data fetching process

--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -69,10 +69,6 @@ const GroupPage = () => {
     enabled: !!groupId,
     staleTime: 1000 * 60 * 2, // 2 minutes
     gcTime: 1000 * 60 * 5, // 5 minutes
-    throwOnError: () => {
-      navigate(paths.LISTENERS(null));
-      return false;
-    },
     retry: false,
   });
 
